5be3cd3420560fc56a56024f1d336c6db56658ac
LuanRM1 <luan.mello@sou.inteli.edu.br>
2023-08-10 15:50:10 -0300

    add a verificação de login
    jadsnjajndasjns

--- front checkin/app.js
+++ front checkin/app.js
@@ -1,73 +1,133 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+
+// Configurações
+const dbPath = 'database/database.db';
+const port = 6969;
+const hostname = 'localhost';
+
+// Inicialização
 const app = express();
-const dbPath = 'database/database.db'; // Substitua pelo caminho desejado para o seu banco de dados
-const port = 6969
-const hostname = 'localhost'
-const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
-    if (err) {
-      console.error('Erro ao abrir o banco de dados:', err.message);
-    } else {
-      console.log('Banco de dados aberto com sucesso!');
+const db = new sqlite3.Database(
+    dbPath,
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+        if (err) {
+            console.error('Erro ao abrir o banco de dados:', err.message);
+        } else {
+            console.log('Banco de dados aberto com sucesso!');
+        }
     }
-  });
+);
+
+// Middlewares
+app.use(express.json());
 app.use(express.static("frontend"));
 
-//endpoint que leva para home.html
+// Endpoints
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/register.html'));
 });
-app.use(express.json());
-app.use(express.static("/frontend"));
-app.listen(port, hostname, () => { // Ligação com o servidor
-    console.log(`Server listening on http://${hostname}:${port}/`);
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/homepageLogged.html'));
+
 });
-  const checkTableExistsQuery = `
-  SELECT name FROM sqlite_master
-    WHERE type='table' AND name='usuarios'
+
+app.post('/register', (req, res) => {
+    const insert = 'INSERT INTO usuarios (email, senha) VALUES (?, ?)';
+    db.run(insert, [req.body.email, req.body.senha], (err) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: 'Failed to register user!' });
+        } else {
+            res.status(201).json({ message: 'User registered successfully!' });
+        }
+    });
+});
+
+app.get('/dados', (req, res) => {
+    const query = "SELECT * FROM usuarios";
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ error: 'Erro ao executar consulta' });
+        }
+
+        // Não feche o banco de dados aqui, pois outros endpoints podem precisar dele.
+        // db.close();
+
+        res.json(rows);
+    });
+});
+
+// Configuração do banco de dados
+const checkTableExistsQuery = `
+    SELECT name FROM sqlite_master WHERE type='table' AND name='usuarios';
 `;
 
 db.get(checkTableExistsQuery, (err, row) => {
-  if (err) {
-    console.error('Erro ao verificar a tabela:', err.message);
-    return;
-  }
+    if (err) {
+        console.error('Erro ao verificar a tabela:', err.message);
+        return;
+    }
 
-  if (!row) {
-    // A tabela 'usuarios' não existe, então criamos ela
-    const createTableQuery = `
-      CREATE TABLE usuarios (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT NOT NULL,
-        senha TEXT NOT NULL
-      )
-    `;
+    if (!row) {
+        const createTableQuery = `
+            CREATE TABLE usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL,
+                senha TEXT NOT NULL
+            );
+        `;
 
-    db.run(createTableQuery, [], (err) => {
-      if (err) {
-        console.error('Erro ao criar a tabela:', err.message);
-      } else {
-        console.log('Tabela "usuarios" criada com sucesso!');
-      }
-    });
-  } else {
-    console.log('A tabela "usuarios" já existe.');
-  }
-
+        db.run(createTableQuery, [], (err) => {
+            if (err) {
+                console.error('Erro ao criar a tabela:', err.message);
+            } else {
+                console.log('Tabela "usuarios" criada com sucesso!');
+            }
+        });
+    }
 });
-app.post('/register', (req, res) => { 
-  const insert = 'INSERT INTO usuarios (email, senha) VALUES (?, ?)';
-  db.run(insert,[req.body.email, req.body.senha], (err) => {
+
+app.post('/verify', (req, res) => {
+  const { email, senha } = req.body; // Extrai email e senha da requisição
+
+  const query = "SELECT * FROM usuarios WHERE email = ?";
+  db.get(query, [email], (err, row) => {
       if (err) {
           console.error(err.message);
-          res.status(500).json({
-              error: 'Failed to register user!'
-          });
-      } else {
-          res.status(201).json({
-              message: 'User registered successfully!'
-          });
+          return res.status(500).json({ error: 'Erro ao executar consulta' });
       }
+      console.log(row)
+      if (!row) {
+          // Se não encontrarmos nenhum usuário com esse email
+          console.log('Usuário não encontrado')
+          return res.status(401).json({ error: 'Credenciais inválidas' });
+      }
+
+      // Neste exemplo, a senha está sendo armazenada em texto simples no banco de dados,
+      // o que não é seguro. Na prática, você deve usar um hashing seguro, como bcrypt, para
+      // armazenar e verificar senhas.
+      if (senha !== row.senha) {
+          return res.status(401).json({ error: 'Credenciais inválidas' });
+      }
+
+      // Se chegarmos até aqui, as credenciais são válidas
+      console.log('Login bem-sucedido')
+      res.status(200).json({ message: 'Login bem-sucedido' });
   });
+});
+
+
+
+
+
+
+
+// Inicialização do servidor
+app.listen(port, hostname, () => {
+    console.log(`Server listening on http://${hostname}:${port}/`);
 });
--- front checkin/frontend/script.js
+++ front checkin/frontend/script.js
@@ -12,6 +12,36 @@ fetch('http://localhost:6969/register', {
             }
         })
         .then(response => response.json())
-        .then(data => { })
+        .then(data => {})
         .catch(err => console.log(err));    
 }
+var dataLogin = {
+    email: document.getElementById('usernameLogin').value,
+    senha: document.getElementById('passwordLogin').value
+};
+
+function verifyLogin() {
+    fetch('http://localhost:6969/verify', { // presumindo que /login é o endpoint correto
+        method: 'POST',
+        body: JSON.stringify(dataLogin),
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw new Error('Erro de autenticação');
+        }
+    })
+    .then(data => {
+        // Aqui você pode redirecionar o usuário para outra página ou atualizar a UI
+        // Por exemplo: window.location.href = "/dashboard";
+    })
+    .catch(err => {
+        console.log(err);
+        // Aqui, você pode informar o usuário sobre um erro de login, por exemplo, através de uma mensagem na tela.
+    });
+}
+
